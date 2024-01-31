1ea90178fdcefd79505b8c3c9a5da7ff37bbd294
Shion Ichikawa <shion1305@gmail.com>
2024-02-01 06:33:28 +0900

    🔧 alter aurora for dev

--- terraform/aurora.tf
+++ terraform/aurora.tf
@@ -5,21 +5,25 @@ variable db_password {}
 resource "aws_rds_cluster" "aurora_cluster" {
   engine                          = "aurora-mysql"
   engine_version                  = "8.0.mysql_aurora.3.04.1"
-  #  db_subnet_group_name            = aws_db_subnet_group.example.name // optional
+  db_subnet_group_name            = aws_db_subnet_group.aurora_db_subnet_group.name
   database_name                   = var.db_name
   master_username                 = var.db_username
   master_password                 = var.db_password
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.custom_rds_profile.name
   vpc_security_group_ids          = [aws_security_group.aurora_sg.id]
+
+  skip_final_snapshot = true
+  apply_immediately   = true
 }
 
 resource "aws_rds_cluster_instance" "aurora_instance" {
-  count               = 1
-  identifier          = "aurora-instance-${count.index}"
-  cluster_identifier  = aws_rds_cluster.aurora_cluster.id
-  instance_class      = "db.t3.small"
-  engine              = aws_rds_cluster.aurora_cluster.engine
-  engine_version      = aws_rds_cluster.aurora_cluster.engine_version
+  count              = 1
+  identifier         = "aurora-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = "db.t3.small"
+  engine             = aws_rds_cluster.aurora_cluster.engine
+  engine_version     = aws_rds_cluster.aurora_cluster.engine_version
+
   publicly_accessible = true
 }
 
@@ -45,5 +49,10 @@ resource "aws_rds_cluster_parameter_group" "custom_rds_profile" {
     name         = "max_sp_recursion_depth"
     value        = "255"
     apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name  = "time_zone"
+    value = "Asia/Tokyo"
   }
 }
