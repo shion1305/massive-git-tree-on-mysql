9405c3ba4fc20b6ac28852253cf2bcca1aa3cacf
Shion Ichikawa <shion1305@gmail.com>
2024-02-01 07:55:31 +0900

    🔧 add eIP, NAT, RouteTables

--- terraform/network.tf
+++ terraform/network.tf
@@ -23,12 +23,60 @@ resource "aws_subnet" "aurora_vpc_subnet2" {
   cidr_block        = "10.0.2.0/24"
 }
 
+# Elastic IP
+# https://www.terraform.io/docs/providers/aws/r/eip.html
+resource "aws_eip" "aurora_nat_eip_1a" {
+  vpc = true
+}
+
+resource "aws_eip" "aurora_nat_eip_1c" {
+  vpc = true
+}
+
+# NAT Gateway
+# https://www.terraform.io/docs/providers/aws/r/nat_gateway.html
+resource "aws_nat_gateway" "aurora_nat_1a" {
+  subnet_id     = aws_subnet.aurora_vpc_subnet1.id
+  allocation_id = aws_eip.aurora_nat_eip_1a.id
+}
+
+resource "aws_nat_gateway" "aurora_nat_1c" {
+  subnet_id     = aws_subnet.aurora_vpc_subnet2.id
+  allocation_id = aws_eip.aurora_nat_eip_1c.id
+}
+
 resource "aws_db_subnet_group" "aurora_db_subnet_group" {
   name       = "aurora_db_subnet"
   subnet_ids = [
     aws_subnet.aurora_vpc_subnet1.id,
     aws_subnet.aurora_vpc_subnet2.id,
   ]
+}
+
+# Route Table
+# https://www.terraform.io/docs/providers/aws/r/route_table.html
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.aurora_vpc.id
+}
+
+# Route
+# https://www.terraform.io/docs/providers/aws/r/route.html
+resource "aws_route" "aurora_public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.aurora_internet_gateway.id
+}
+
+# Association
+# https://www.terraform.io/docs/providers/aws/r/route_table_association.html
+resource "aws_route_table_association" "aurora_public_1a" {
+  subnet_id      = aws_subnet.aurora_vpc_subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "aurora_public_1c" {
+  subnet_id      = aws_subnet.aurora_vpc_subnet2.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "aurora_sg" {
